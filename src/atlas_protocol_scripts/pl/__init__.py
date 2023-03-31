371763b5ab47c9657f9358d86f719cca70eb0431
Dietmar Rieder <dietmar.rieder@i-med.ac.at>
2023-03-31 15:56:45 +0200

    add utils

--- src/atlas_protocol_scripts/pl/__init__.py
+++ src/atlas_protocol_scripts/pl/__init__.py
@@ -1,3 +1,5 @@
 from ._qc import plot_qc_metrics
+from ._util import reshape_clustermap, save_fig_mfmt
 
-__all__ = ["plot_qc_metrics"]
+
+__all__ = ["plot_qc_metrics", "reshape_clustermap", "save_fig_mfmt"]

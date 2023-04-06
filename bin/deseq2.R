e8b3e1b34d5ac800f3f59fb14176e0053c96d278
Erika Kvalem <erika.kvalem@student.i-med.ac.at>
2023-04-06 12:00:56 +0200

    deseq2

--- bin/deseq2.R
+++ bin/deseq2.R
@@ -1,39 +1,51 @@
 #!/usr/bin/env Rscript
 
+'deseq2.R
+
+Usage:
+  deseq2.R <sample_sheet> <count_table> --c1=<c1> --c2=<c2> [options]
+  deseq2.R --help
+
+Arguments:
+  <sample_sheet>                CSV file with the sample annotations.
+  <count_table>                 TSV file with the read counts
+
+Mandatory options:
+  --c1=<c1>                     Contrast level 1 (perturbation). Needs to be contained in condition_col.
+  --c2=<c2>                     Contrast level 2 (baseline). Needs to be contained in condition_col.
+
+
+' -> doc
+
+
 library("DESeq2")
-#library("docopt")
-#arguments = docopt(doc, version = "0.1")
-
-# Testdata
-# Example1
-sampleAnnotationCSV = "/data/projects/2023/atlas_protocol/results/differential_expression/Tumorcells_samplesheet.csv"
-readCountFile = "/data/projects/2023/atlas_protocol/results/differential_expression/Tumorcells_counts.tsv"
-result_dir ="/data/projects/2023/atlas_protocol/results/differential_expression"
-cond_col = "group"
-sample_col = "sample"
-contrast = c("group", "LUAD", "LUSC")
-covariate_formula = ""
-
-
+library("docopt")
+arguments = docopt
 
 # Input and output
-#sampleAnnotationCSV <- arguments$sample_sheet
-#readCountFile <- arguments$count_table
-#results_dir = arguments$result_dir
-#contrast = c(cond_col, arguments$c1, arguments$c2)
-#covariate_formula = arguments$covariate_formula
+sampleAnnotationCSV <- arguments$sample_sheet
+readCountFile <- arguments$count_table
+cond_col = "group"
+sample_col = "sample"
+covariate_formula = ""
+contrast = c(cond_col, arguments$c1, arguments$c2)
 
-#count_mat <- read_tsv(readCountFile, row.names="gene_symbol")
-#allSampleAnno <- read_csv(sampleAnnotationCSV,  row.names=1)
+# Testdata
+#sampleAnnotationCSV = "/data/projects/2023/atlas_protocol/results/differential_expression/Tumorcells_samplesheet.csv"
+#readCountFile = "/data/projects/2023/atlas_protocol/results/differential_expression/Tumorcells_counts.tsv"
+#result_dir ="/data/projects/2023/atlas_protocol/results/differential_expression"
+#cond_col = "group"
+#sample_col = "sample"
+#contrast = c("group", "LUAD", "LUSC")
+#covariate_formula = ""
+
 
 count_mat <- as.matrix(read.csv(readCountFile,sep="\t",row.names="gene_symbol"))
 count_mat <- as.data.frame(count_mat)
 allSampleAnno <- read.csv(sampleAnnotationCSV, row.names=1)
 
+colnames(count_mat) <- NULL
 count_mat[,-1]= round(count_mat[,-1],0) 
-
-#sampleAnno <- allSampleAnno %>%
-#  filter(get(cond_col) %in% contrast[2:3])
 
 design_formula <- as.formula(paste0("~", cond_col, covariate_formula))
 

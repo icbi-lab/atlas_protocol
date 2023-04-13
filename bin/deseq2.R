feb853ba81b0f625eddd4c4d4ef8b24c18141051
Erika Kvalem <erika.kvalem@student.i-med.ac.at>
2023-04-13 21:42:43 +0200

    bot change

--- bin/deseq2.R
+++ bin/deseq2.R
@@ -1,75 +1,125 @@
 #!/usr/bin/env Rscript
 
-'deseq2.R
-
-Usage:
-  deseq2.R <sample_sheet> <count_table> --c1=<c1> --c2=<c2> [options]
-  deseq2.R --help
-
-Arguments:
-  <sample_sheet>                CSV file with the sample annotations.
-  <count_table>                 TSV file with the read counts
-
-Mandatory options:
-  --c1=<c1>                     Contrast level 1 (perturbation). Needs to be contained in condition_col.
-  --c2=<c2>                     Contrast level 2 (baseline). Needs to be contained in condition_col.
-
-
-' -> doc
-
-
+#Libraries
 library("DESeq2")
-library("docopt")
-arguments = docopt
-
-# Input and output
-sampleAnnotationCSV <- arguments$sample_sheet
-readCountFile <- arguments$count_table
-cond_col = "group"
-sample_col = "sample"
-covariate_formula = ""
-contrast = c(cond_col, arguments$c1, arguments$c2)
-
-# Testdata
-#sampleAnnotationCSV = "/data/projects/2023/atlas_protocol/results/differential_expression/Tumorcells_samplesheet.csv"
-#readCountFile = "/data/projects/2023/atlas_protocol/results/differential_expression/Tumorcells_counts.tsv"
-#result_dir ="/data/projects/2023/atlas_protocol/results/differential_expression"
-#cond_col = "group"
-#sample_col = "sample"
-#contrast = c("group", "LUAD", "LUSC")
-#covariate_formula = ""
+library("BiocParallel")
+library("org.Mm.eg.db")
+library("dplyr")
+library("IHW")
+library("tibble")
+library("readr")
+library("argparser", quietly = TRUE)
+library(conflicted)
+library(tidyverse)
+conflict_prefer("select", "dplyr")
 
 
-count_mat <- as.matrix(read.csv(readCountFile,sep="\t",row.names="gene_symbol"))
-count_mat <- as.data.frame(count_mat)
-allSampleAnno <- read.csv(sampleAnnotationCSV, row.names=1)
+# Parser
+p <- arg_parser("Input Differential Expression Analysis")
 
-<<<<<<< HEAD
+# Arguments
+p <- add_argument(p, "countData", help = "Count matrix", type = "character")
+p <- add_argument(p, "colData", help = "Sample annotation matrix", type = "character")
+p <- add_argument(p, "--resDir", help = "Output result directory", default = "./results")
+p <- add_argument(p, "--cond_col", help = "Condition column in sample annotation", default = "group")
+p <- add_argument(p, "--covariate_formula", help = "Additional covariates that will be added to the design formula", default = "")
+p <- add_argument(p, "--prefix", help = "Prefix of result file", default = "test")
+p <- add_argument(p, "--sample_col", help = "Sample identifier in sample annotation", default = "sample")
+p <- add_argument(p, "--fdr", help = "False discovery rate", default = 0.1)
+p <- add_argument(p, "--c1", help = "Contrast level 1 (perturbation)", default = "grpA")
+p <- add_argument(p, "--c2", help = "Contrast level 2 (baseline)", default = "grpB")
+p <- add_argument(p, "--cpus", help = "Number of cpus", default = 8)
+p <- add_argument(p, "--save_workspace", help = "Save R workspace", flag = TRUE)
 
-=======
->>>>>>> d93b123eddc5ce9de7f8099eed3a66622d5218d6
-count_mat[,-1]= round(count_mat[,-1],0)
 
+# # For testing only
+# argv = environment()
+# resDir = argv$results_dir = "/data/projects/2023/atlas_protocol/results/differential_expression"
+# prefix = argv$prefix = "test"
+# cond_col  = argv$cond_col = "condition"
+# sample_col  = argv$sample_col = "sample"
+# fdr_cutoff  = argv$fdr = 0.1
+# c1  = argv$c1 = "LUAD"
+# c2  = argv$c2 = "LUSC"
+# argv$cpus = 8
+# argv$covariate_formula = ""
+# count_mat_path  = argv$countData ="/data/projects/2023/atlas_protocol/results/differential_expression/deseq_resdir/LUSC_vs_LUAD_B_cell_adata.counts.csv"
+# sampleAnno_path = argv$colData = "/data/projects/2023/atlas_protocol/results/differential_expression/deseq_resdir/LUSC_vs_LUAD_B_cell_adata.samplesheet.csv"
+
+
+# Parse cmd line arguments
+argv <- parse_args(p)
+
+sampleAnno_path = argv$colData
+count_mat_path = argv$countData
+resDir <- argv$resDir
+prefix <- argv$prefix
+cond_col <- argv$cond_col
+sample_col <- argv$sample_col
+fdr_cutoff <- argv$fdr
+c1 <- argv$c1
+c2 <- argv$c2
+n_cpus <- argv$cpus
+save_ws = argv$save_workspace
+covariate_formula <- argv$covariate_formula
+
+# Number of cpus to be used
+register(MulticoreParam(workers = n_cpus))
+
+# Reading the Annotation sample csv file
+sampleAnno <- read_csv(sampleAnno_path)
+rownames(sampleAnno) <- sampleAnno$index
+
+# Reading the Count matrix tsv file
+count_mat <- read_csv(count_mat_path)
+count_mat <- count_mat |>
+  select(c(gene_id, sampleAnno[[sample_col]])) |>
+  column_to_rownames("gene_id") |>
+  round()
+
+gene_id <- count_mat$gene_id
+count_mat <- count_mat[,-1]
+rownames(count_mat) <-  gene_id
+
+
+if (!all(rownames(sampleAnno) %in% colnames(count_mat))) {
+  print('Row names in sample annotation and column names in count matrix are not the same')
+  break
+}
+
+# Start processing
 design_formula <- as.formula(paste0("~", cond_col, covariate_formula))
 
-
-################# Start processing
-dds <- DESeqDataSetFromMatrix(countData = count_mat,
-                              colData = allSampleAnno,
+dds <- DESeqDataSetFromMatrix(countData = round(count_mat),
+                              colData = sampleAnno,
                               design = design_formula)
-# run DESeq
-dds.res<- DESeq(dds)
 
+## keep only genes where  >= 10 reads per sample condition in total
+keep <- rowSums(counts(collapseReplicates(dds, dds[[cond_col]]))) >= 10
+dds <- dds[keep, ]
+
+# run DESeq
+n_cpus = 6
+dds.res<- DESeq(dds, parallel = (n_cpus > 1))
+
+contrasts = list(c(cond_col, c1, c2))
+names(contrasts) = sprintf("%s_vs_%s", c1, c2)
 ### IHW
 
 # use of IHW for p value adjustment of DESeq2 results
-resIHW <- results(dds.res, filterFun=ihw, contrast=contrast)
+resIHW = lapply(names(contrasts), function(name) {
+  contrast = contrasts[[name]]
+  results(dds.res, filterFun = ihw, contrast = contrast) |>
+    as_tibble(rownames = "gene_id") |>
+    mutate(comparison = name) |>
+    arrange(pvalue)
+}) |> bind_rows()
 
-resIHW <- as.data.frame(resIHW ) |>
-  rownames_to_column(var = "gene_id") |>
-  as_tibble() |>
-  arrange(padj)
 
+# write results to TSV and XLSX files
+write_tsv(resIHW, file.path(resDir, paste0(prefix, "_DESeq2_result.tsv")))
 
-#### write results to TSV and XLSX files
-write.csv(resIHW , "/data/projects/2023/atlas_protocol/results/differential_expression/IHWallGenes.tsv", row.names=FALSE)
+# Save R ws
+if (save_ws) {
+  save.image(file = file.path(resDir, paste0(prefix, "_ws.RData")))
+}

fb231383683d85fe1e0f001266dae15cea73fbb0
Gregor Sturm <mail@gregor-sturm.de>
2023-04-13 10:15:44 +0200

    Cellphonedb analysis (#12)
    * Add custom scripts for cellphonedb analysis to lib
    
    * Add notebook
    
    * fix circular import
    
    * Add description
    
    * fix sphinx
    
    * reproducible results using test data
    
    * reproducible results using test data
    
    * typo
    
    * revert [5.] from </> to 'markdown'
    
    * minor visualisation fix
    
    * hotfix graph
    
    * references
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove output paths
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * resolved the H2 headers
    
    ---------
    
    Co-authored-by: abyssum <fotakisg@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/atlas_protocol_scripts/tl/__init__.py
+++ src/atlas_protocol_scripts/tl/__init__.py
@@ -1,7 +1,6 @@
+from ._cell2cell import CpdbAnalysis
 from ._fdr import fdr_correction
+from ._pseudobulk import pseudobulk
 from ._scissor import scissor_wilcoxon_test
 
-__all__ = [
-    "fdr_correction",
-    "scissor_wilcoxon_test",
-]
+__all__ = ["fdr_correction", "scissor_wilcoxon_test", "pseudobulk"]

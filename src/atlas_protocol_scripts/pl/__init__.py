b168bfaf510724b0c97e281167ec552f99488b79
Dietmar Rieder <dietmar.rieder@i-med.ac.at>
2023-04-14 13:52:37 +0200

    fix __init__

--- src/atlas_protocol_scripts/pl/__init__.py
+++ src/atlas_protocol_scripts/pl/__init__.py
@@ -1,8 +1,4 @@
-from ._qc import plot_qc_metrics
+from ._significance_heatmap import significance_heatmap
 from ._util import reshape_clustermap, save_fig_mfmt
 
-__all__ = ["plot_qc_metrics", "reshape_clustermap", "save_fig_mfmt"]
-
-from ._significance_heatmap import significance_heatmap
-
-__all__ = ["significance_heatmap"]
+__all__ = ["significance_heatmap", "reshape_clustermap", "save_fig_mfmt"]

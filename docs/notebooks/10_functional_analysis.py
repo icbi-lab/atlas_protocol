7b627699c37d8b002c3ca43ea1f0eec4f5961ae0
Dietmar Rieder <dietmar.rieder@i-med.ac.at>
2023-04-18 16:33:39 +0200

    Restrict number of barplots in notebook. Switch to GSEA for MSigDB

--- docs/notebooks/10_functional_analysis.py
+++ docs/notebooks/10_functional_analysis.py
@@ -47,6 +47,7 @@ import numpy as np
 import pandas as pd
 import scanpy as sc
 import seaborn as sns
+import statsmodels.stats.multitest
 from IPython.display import display
 from pydeseq2.dds import DeseqDataSet
 from pydeseq2.ds import DeseqStats
@@ -343,8 +344,15 @@ for contrast in contrasts:
 # We use the decoupler `plot_barplot` function for plotting the pathway activities of each celltype and save the result in `png, pdf, svg` format.
 
 # %%
+# show maximum n plots in notebook, all are saved as files
+show_n = 2
+
 for contrast in contrasts:
     print(contrast["name"])
+
+    plt.ioff()
+    p_count = 0
+
     for ct in cell_types:
         bp = dc.plot_barplot(contrast["pathway_acts"], ct, top=25, vertical=False, return_fig=True, figsize=[5, 3])
         plt.title(ct)
@@ -359,6 +367,12 @@ for contrast in contrasts:
                 fmt="all",
                 plot_provider="mpl",
             )
+            # Only show the first two plots in the notebook
+            p_count += 1
+            if p_count <= show_n:
+                display(bp)
+
+            plt.close()
         else:
             print("No plot for: " + contrast["name"] + ":" + ct)
 
@@ -502,8 +516,15 @@ for contrast in contrasts:
 # We use the decoupler `plot_barplot` function for plotting the transcription factor activities of each celltype and save the result in `png, pdf, svg` format.
 
 # %%
+# show maximum n plots in notebook, all are saved as files
+show_n = 2
+
 for contrast in contrasts:
     print(contrast["name"])
+
+    plt.ioff()
+    p_count = 0
+
     for ct in cell_types:
         bp = dc.plot_barplot(contrast["tf_acts"], ct, top=25, vertical=False, return_fig=True, figsize=[5, 3])
         plt.title(ct)
@@ -518,6 +539,14 @@ for contrast in contrasts:
                 fmt="all",
                 plot_provider="mpl",
             )
+
+            # Only show the first two plots in the notebook
+            p_count += 1
+            if p_count <= show_n:
+                display(bp)
+
+            plt.close()
+
         else:
             print("No plot for: " + contrast["name"] + ":" + ct)
 
@@ -532,8 +561,8 @@ for contrast in contrasts:
 # generate heatmap plot
 for contrast in contrasts:
     # get acts and pvals
-    pvals = contrast["tf_pvals"]
-    acts = contrast["tf_acts"]
+    pvals = contrast["tf_pvals"].copy()
+    acts = contrast["tf_acts"].copy()
 
     # select the columns that have significant acts (pval < 0.05)
     sig_pval_cols = pvals.columns[(pvals < 0.05).any()]
@@ -659,78 +688,91 @@ for contrast in contrasts:
 
 
 # %% [markdown]
-# ## Infer enrichment of biological terms with ORA using significant differential expressed genes
+# ## Infer enrichment of biological terms with GSEA using significant differential expressed genes
 #
-# We can utilize MSigDB to assign biological terms to the differentially expressed genes. In this case, we will employ the `get_ora_df` method from decoupler.
-
-# %%
-# convert deseq2 FDR results to long format
-for contrast in contrasts:
-    fdr_long = pd.melt(
-        contrast["fdr_mat"].T.rename_axis("gene_symbol").reset_index(),
-        id_vars=["gene_symbol"],
-        var_name="cell_type",
-        value_name="padj",
-    )
-
-    # Get top genes (FDR < 0.05)
-    top_genes = fdr_long[fdr_long["padj"] < 0.05]
-    contrast["top_genes"] = top_genes
+# We can utilize MSigDB to assign biological terms to the differentially expressed genes. In this case, we will employ the `run_gsea` method from decoupler.
 
 # %% [markdown]
-# ### Run ORA
-#
-# We set 0 ORA p-values to min(p-value)/10 to avoid inf values when log transforming while calculating the ora_score
+# ### Run GSEA
+# We use the t-value (Wald test) from the DESeq2 result to rank the genes.
 
 # %%
-# Infer enrichment with ora using significant deg
 for contrast in contrasts:
-    enr_pvals = dc.get_ora_df(
-        contrast["top_genes"],
+    # run_gsea from decoupler
+    gsea_estimate, gsea_norm, gsea_pvals = dc.run_gsea(
+        contrast["stat_mat"],
         msigdb,
-        groupby="cell_type",
-        features="gene_symbol",
         source="geneset",
         target="genesymbol",
+        times=1000,
+        batch_size=10000,
+        min_n=5,
+        seed=4711,
+        verbose=False,
     )
 
-    # set enrichment p-values 0 to min/10 p-values to avoid inf values when log transforming
-    enr_pvals.values[enr_pvals.values == 0] = np.min(enr_pvals.values[enr_pvals.values != 0]) / 10
-
-    contrast["enr_pvals"] = enr_pvals
-    display(enr_pvals)
+    contrast["gsea_estimate"] = gsea_estimate
+    contrast["gsea_norm"] = gsea_norm
+    contrast["gsea_pvals"] = gsea_pvals
 
 # %% [markdown]
-# ### Generate MSigDB heatmap from ORA scores
-#
-# We use the seaborn `heatmap` function to generate a heatmap of the biological terms assigned by ORA to the different celltypes and save the result in `png, pdf, svg` format.\
-# Signigficantly overrepresented terms are marked with "●"
-# * ORA score is -log10(ora p-value)
+# Now we correct for multiple hypothesis testing using BH
 
 # %%
-# Plot heatmap of ORA result
 for contrast in contrasts:
-    enr_pvals = contrast["enr_pvals"]
+    # make long format
+    gsea_pvals = contrast["gsea_pvals"]
+    gsea_pvals_long = pd.melt(
+        gsea_pvals.T.rename_axis("source").reset_index(),
+        id_vars=["source"],
+        var_name="cell_type",
+        value_name="pval",
+    )
 
-    # calculate ORA score = -log10(ora_pval)
-    ora_scores = -np.log10(enr_pvals)
-    contrast["ora_scores"] = ora_scores
+    # run fdrcorrection and add padj to df
+    gsea_pvals_long["padj"] = statsmodels.stats.multitest.fdrcorrection(gsea_pvals_long["pval"].values)[1]
 
-    # mark significant activities
-    sig_mat = np.where(enr_pvals < 0.05, "●", "")
+    # make padj_mat
+    gsea_padj = gsea_pvals_long.pivot(index="cell_type", columns="source", values="padj")
+    gsea_padj.index.name = None
+    # make sure that we preserver the order of row and columns
+    contrast["gsea_padj"] = gsea_padj.reindex(index=contrast["gsea_pvals"].index).reindex(
+        contrast["gsea_pvals"].columns, axis=1
+    )
+
+
+# %% [markdown]
+# ### Generate MSigDB heatmap from GSEA normalized enrichment scores
+#
+# We use the seaborn `heatmap` function to generate a heatmap of the biological terms assigned by GSEA to the different celltypes and save the result in `png, pdf, svg` format.\
+# Signigficantly overrepresented terms are marked with "●"
+# * NES (normalized enrichment score) is color-coded
+
+# %%
+# Plot heatmap of GSEA result
+for contrast in contrasts:
+    gsea_padj = contrast["gsea_padj"]
+
+    gsea_norm = contrast["gsea_norm"].copy()
+
+    # filter for enriched (positve nes score)
+    gsea_norm[gsea_norm < 0] = 0
+
+    # mark significant enrichment
+    sig_mat = np.where(gsea_padj[gsea_norm > 0] < 0.05, "●", "")
 
     # create figure
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(17, 10))
 
     # create heatmap
     hm = sns.heatmap(
-        ora_scores,
+        gsea_norm,
         annot=sig_mat,
         robust=True,
         cmap="viridis",
         linewidth=0.5,
         cbar=True,
-        cbar_kws={"label": "ORA score", "shrink": 0.4},
+        cbar_kws={"label": "normalized enrichment score", "shrink": 0.4},
         fmt="s",
         square=True,
         xticklabels=True,
@@ -740,7 +782,7 @@ for contrast in contrasts:
     aps.pl.save_fig_mfmt(
         fig,
         res_dir=f"{contrast['res_dir']}/MSigDB/",
-        filename=f"{contrast['name']}_MSigDB_ORA_heatmap",
+        filename=f"{contrast['name']}_MSigDB_GSEA_heatmap",
         fmt="all",
         plot_provider="mpl",
     )
@@ -750,26 +792,45 @@ for contrast in contrasts:
 # ### Visualize the most enriched terms as barplot
 #
 # We use the decoupler `plot_barplot` function to generate barplots of the most enriched biological and save the result in `png, pdf, svg` format.
-# * `top_n`: top n cell types to generate barplots for. Cell types are sorted by the highest ORA score
+# * `top_n`: top n cell types to generate barplots for. Cell types are sorted by the highest GSEA score
 
 # %%
-top_n = 6
+top_n = 4
 
-# %%
 # show top n celltypes
-np.max(contrast["ora_scores"].T).sort_values().tail(top_n)
+for contrast in contrasts:
+    print(contrast["name"] + "\n")
+    display(np.max(contrast["gsea_norm"].T).sort_values().tail(top_n))
+    display(np.min(contrast["gsea_padj"].T).sort_values().head(top_n))
 
 # %%
 for contrast in contrasts:
+    gsea_norm = contrast["gsea_norm"].copy()
+
+    # filter for enriched (positve nes score)
+    gsea_norm[gsea_norm < 0] = 0
+
     # get top n celltypes
-    top_celltypes = np.max(contrast["ora_scores"].T).sort_values().tail(top_n).index.values
+    top_celltypes = np.min(gsea_padj.T).sort_values().head(top_n).index.values
 
     with plt.rc_context({"figure.figsize": (8, 3)}):
         for ct in top_celltypes:
-            bp = dc.plot_barplot(contrast["ora_scores"], ct, top=15, vertical=True, return_fig=True, figsize=[8, 3])
+            vcenter = np.max(gsea_norm.T[ct]) / 2
+            bp = dc.plot_barplot(
+                gsea_norm,
+                ct,
+                top=10,
+                vertical=True,
+                return_fig=True,
+                vmin=0,
+                vcenter=vcenter,
+                cmap="Reds",
+                figsize=[8, 3],
+            )
             plt.title(ct)
-            plt.xlabel("ORA score")
+            plt.xlabel("normalized enrichment score")
             plt.tight_layout()
+            plt.show()
 
             if bp is not None:
                 ct_fname = ct.replace(" ", "_").replace("/", "_")
@@ -782,6 +843,7 @@ for contrast in contrasts:
                 )
             else:
                 print("No plot for: " + contrast["name"] + ":" + ct)
+plt.close()
 
 
 # %% [markdown]
@@ -794,7 +856,7 @@ top_n = 6
 
 # %%
 # show top n celltypes
-np.max(contrast["ora_scores"].T).sort_values().tail(top_n)
+np.max(contrast["gsea_norm"].T).sort_values().tail(top_n)
 
 # %%
 for contrast in contrasts:
@@ -802,12 +864,12 @@ for contrast in contrasts:
     pvals = contrast["fdr_mat"]
 
     # get top n celltypes
-    top_celltypes = np.max(contrast["ora_scores"].T).sort_values().tail(top_n).index.values
+    top_celltypes = np.min(contrast["gsea_padj"].T).sort_values().head(top_n).index.values
 
     # get top term of each celltype
     top_term = {}
     for ct in top_celltypes:
-        top_term[ct] = contrast["ora_scores"].loc[ct].idxmax()
+        top_term[ct] = contrast["gsea_norm"].loc[ct].idxmax()
 
     # Calculate nrows based on ncol
     ncols = 3 if top_n >= 3 else n_sig
@@ -852,16 +914,19 @@ for contrast in contrasts:
     )
 
 # %% [markdown]
-# ### Save ORA scores of enriched terms
+# ### Save GSEA scores of enriched terms
 #
-# Finally we store the ORA scores of enriched terms in `tsv` format.
+# Finally we store the GSEA scores of enriched terms in `tsv` format.
 
 # %%
 # save tsv
 for contrast in contrasts:
     tsv_dir = Path(contrast["res_dir"], "MSigDB", "tsv")
     os.makedirs(tsv_dir, mode=0o750, exist_ok=True)
-    contrast["ora_scores"].to_csv(f"{tsv_dir}/{contrast['name']}_MSigDB_ORA_scors.tsv", sep="\t")
+    contrast["gsea_norm"].to_csv(f"{tsv_dir}/{contrast['name']}_MSigDB_GSEA_nes.tsv", sep="\t")
+    contrast["gsea_estimate"].to_csv(f"{tsv_dir}/{contrast['name']}_MSigDB_GSEA_estimate.tsv", sep="\t")
+    contrast["gsea_pvals"].to_csv(f"{tsv_dir}/{contrast['name']}_MSigDB_GSEA_pvals.tsv", sep="\t")
+    contrast["gsea_padj"].to_csv(f"{tsv_dir}/{contrast['name']}_MSigDB_GSEA_padj.tsv", sep="\t")
 
 
 # %% [markdown]
@@ -902,8 +967,8 @@ for contrast in contrasts:
 # generate heatmap plot
 for contrast in contrasts:
     # get acts and pvals
-    pvals = contrast["cs_pvals"]
-    acts = contrast["cs_acts"]
+    pvals = contrast["cs_pvals"].copy()
+    acts = contrast["cs_acts"].copy()
 
     # select the columns that have significant acts (pval < 0.05)
     sig_pval_cols = pvals.columns[(pvals < 0.05).any()]
